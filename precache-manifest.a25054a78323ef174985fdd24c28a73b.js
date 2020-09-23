dd477560466be574d231ca8dfe2198a39a468497
madhujella <r.madhuj68@gmail.com>
2020-09-23 15:38:33 +0530

    Updates

---
+++ precache-manifest.a25054a78323ef174985fdd24c28a73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeadd51b22b4ac567e622692ce904b92",
+    "url": "/commuteesTask/index.html"
+  },
+  {
+    "revision": "4d5d24f9c88c60784322",
+    "url": "/commuteesTask/static/css/main.26025c5a.chunk.css"
+  },
+  {
+    "revision": "01419a3881be58e9a031",
+    "url": "/commuteesTask/static/js/2.9a1da22d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/commuteesTask/static/js/2.9a1da22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5d24f9c88c60784322",
+    "url": "/commuteesTask/static/js/main.2a08b056.chunk.js"
+  },
+  {
+    "revision": "8d34166c26a2fd82f425",
+    "url": "/commuteesTask/static/js/runtime-main.028f1b27.js"
+  }
+]);
